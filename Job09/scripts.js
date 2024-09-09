dfde2708142d99bb367a71d5189f6a286145fb93
antony-lucide <113905621+antony-lucide@users.noreply.github.com>
2024-09-09 16:02:48 +0200

    Runtrack

---
+++ Job09/scripts.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+    function tri(table, order) {
+        if (order === "asc") {
+            table.sort((a, b) => a - b);
+        } else if (order === "desc") {
+            table.sort((a, b) => b - a); 
+        }
+        return table;
+    }
+
+   
+    let numbers = [3, 1, 4, 1, 5, 9, 2, 6, 5];
+    console.log(tri(numbers, "asc"));  
+    console.log(tri(numbers, "desc")); 
+}
