644555d50354395fb85d3a68f5651470ec7b998f
antony-lucide <113905621+antony-lucide@users.noreply.github.com>
2024-09-11 16:49:57 +0200

    Runtrack3

--- Jour03/Job02/scripts.js
+++ Jour03/Job02/scripts.js
@@ -1,11 +1,42 @@
 window.onload = function() {
-    let drag = $("#Sort");
+    let drag = $("img");
 
     let btn = $('#btn');
 
-    let offset = $(drag).offset()
-
+    let Win = false
     $(drag).draggable();
 
+    $(btn).bind('click', function(){
+        Math.random(Math.floor(drag))
+    })
+
+
+    $(drag).droppable({
+        accept: "img",
+        drop: function(event, ui) {
+            let droppedItem = ui.helper.clone(); // Clone the dragged item
+            droppedItem.draggable({ containment: "#target" }); // Make the clone draggable within the target container
+            $(this).append(droppedItem); // Append the cloned item to the target container
+            ui.helper.remove(); // Remove the original image from the start container
+
+            checkOrder(); // Check if the images are in the correct order
+        }
+    });
+
+
+    function checkOrder() {
+        let correctOrder = ["id1", "id2", "id3", "id4", "id5", "id6"];
+        let currentOrder = [];
+
+        
+        if(correctOrder.length() == currentOrder.length){
+            Win = true
+            console.log("You win !")
+        } else {
+            Win = false
+            console.log("still not in order")
+        }
+
+    }
     console.log('test');
 }
